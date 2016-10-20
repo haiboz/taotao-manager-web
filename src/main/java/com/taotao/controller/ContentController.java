b7e1795ac7a310bcda515b5d46ecf8157e1422a0
haiboz <798102408@qq.com>
2016-10-20 17:13:40 +0800

    首页大广告位展示 后台内容添加等

--- src/main/java/com/taotao/controller/ContentController.java
+++ src/main/java/com/taotao/controller/ContentController.java
@@ -6,10 +6,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.taotao.common.pojo.EUDataGridResult;
+import com.taotao.common.pojo.TaotaoResult;
+import com.taotao.pojo.TbContent;
 import com.taotao.service.ContentService;
 
 @Controller
-@RequestMapping("content/query")
+@RequestMapping("content")
 public class ContentController {
 		
 	@Autowired 
@@ -20,11 +22,25 @@ public class ContentController {
 	 * @param categoryId
 	 * @return
 	 */
-	@RequestMapping("/list")
+	@RequestMapping("/query/list")
 	@ResponseBody
 	public EUDataGridResult getContentList( Long categoryId,int page,int rows){
 		EUDataGridResult result = contentServiceImpl.getContentList(categoryId,page,rows);
 		return result;
 	}
+	
+	/**
+	 * 新增内容
+	 * @param tbCOntent
+	 * @return
+	 */
+	@RequestMapping("/save")
+	@ResponseBody
+	public TaotaoResult saveContent(TbContent tbContent){
+		TaotaoResult result = contentServiceImpl.saveContent(tbContent);
+		return result;
+	}
+	
+	
 	
 }
